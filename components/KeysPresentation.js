3de964a013d5aea8ba47b9d990f1d7f2ccdb6437
TemTem <t3mir1an@gmail.com>
2022-01-23 20:27:34 -0600

    chrome keystrokes

--- components/KeysPresentation.js
+++ components/KeysPresentation.js
@@ -1,13 +1,41 @@
+import keystrokes from '../utils/keystrokes.json';
+
 const KeysPresentation = ({ pressedKey }) => {
-    // console.log('pressedKeu', pressedKey);
+    console.log('pressedKeu', pressedKey);
     const options = [];
-    if (pressedKey.ctrlKey) options.push('Control');
+    let keystrokeDesc = keystrokes;
+    if (pressedKey.ctrlKey) options.push('Ctrl');
     if (pressedKey.metaKey) options.push('Meta');
     if (pressedKey.altKey) options.push('Alt');
     if (pressedKey.shiftKey) options.push('Shift');
-    if (!['Control', 'Alt', 'Meta', 'Shift'].includes(pressedKey.key)) options.push(pressedKey.key);
+    options.map((optKey) => {
+        keystrokeDesc = keystrokeDesc?.[optKey];
+        // console.log('keystrokeDesc', keystrokeDesc);
+    });
 
-    return <div>{options.join(' + ')}</div>;
+    // console.log('keystrokeDesc', pressedKey?.key?.toLowerCase(), letterKey);
+
+    if (!['Ctrl', 'Meta', 'Alt', 'Shift'].includes(pressedKey.key)) {
+        const letterKey = String.fromCharCode(pressedKey?.keyCode)?.toLocaleLowerCase();
+
+        if (letterKey) {
+            options.push(letterKey);
+            keystrokeDesc = keystrokeDesc?.[letterKey];
+        } else {
+            options.push(pressedKey.key);
+            keystrokeDesc = keystrokeDesc?.[pressedKey.key];
+        }
+    }
+
+    const pressedKeyStroke = options.join(' + ');
+
+    return (
+        <div>
+            <div>
+                {pressedKeyStroke}: {keystrokeDesc?.desc ? JSON.stringify(keystrokeDesc) : ''}
+            </div>
+        </div>
+    );
 };
 
 export default KeysPresentation;
