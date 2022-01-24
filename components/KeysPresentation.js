828c67947c76df46aea7fe143a65e909f3de2e09
TemTem <t3mir1an@gmail.com>
2022-01-23 23:08:56 -0600

    nice looks

--- components/KeysPresentation.js
+++ components/KeysPresentation.js
@@ -1,39 +1,102 @@
 import keystrokes from '../utils/keystrokes.json';
 
 const KeysPresentation = ({ pressedKey }) => {
-    console.log('pressedKeu', pressedKey);
-    const options = [];
-    let keystrokeDesc = keystrokes;
-    if (pressedKey.ctrlKey) options.push('Ctrl');
-    if (pressedKey.metaKey) options.push('Meta');
-    if (pressedKey.altKey) options.push('Alt');
-    if (pressedKey.shiftKey) options.push('Shift');
-    options.map((optKey) => {
-        keystrokeDesc = keystrokeDesc?.[optKey];
-        // console.log('keystrokeDesc', keystrokeDesc);
+    const pressedKeys = [];
+    let done = false;
+    if (pressedKey.metaKey) pressedKeys.push('Meta');
+    if (pressedKey.ctrlKey) pressedKeys.push('Ctrl');
+    if (pressedKey.altKey) pressedKeys.push('Alt');
+    if (pressedKey.shiftKey) pressedKeys.push('Shift');
+
+    let availableKeystrokes = keystrokes;
+    pressedKeys.map((optKey) => {
+        availableKeystrokes = availableKeystrokes?.[optKey];
     });
 
-    // console.log('keystrokeDesc', pressedKey?.key?.toLowerCase(), letterKey);
-
-    if (!['Ctrl', 'Meta', 'Alt', 'Shift'].includes(pressedKey.key)) {
-        const letterKey = String.fromCharCode(pressedKey?.keyCode)?.toLocaleLowerCase();
-
-        if (letterKey) {
-            options.push(letterKey);
-            keystrokeDesc = keystrokeDesc?.[letterKey];
+    if (!['Ctrl', 'Meta', 'Alt', 'Shift', 'Control'].includes(pressedKey.key)) {
+        if (Number(pressedKey?.keyCode) > 64 && Number(pressedKey?.keyCode) < 91) {
+            const letterKey = String.fromCharCode(pressedKey?.keyCode)?.toLocaleLowerCase();
+            pressedKeys.push(letterKey);
+            availableKeystrokes = availableKeystrokes?.[letterKey];
         } else {
-            options.push(pressedKey.key);
-            keystrokeDesc = keystrokeDesc?.[pressedKey.key];
+            pressedKeys.push(pressedKey.key?.toLocaleLowerCase());
+            availableKeystrokes = availableKeystrokes?.[pressedKey.key?.toLocaleLowerCase()];
         }
+        done = true;
     }
 
-    const pressedKeyStroke = options.join(' + ');
+    const isKeystroke =
+        availableKeystrokes?.Chrome ||
+        availableKeystrokes?.Mac ||
+        availableKeystrokes?.desc ||
+        availableKeystrokes?.Windows;
+    const available = !isKeystroke && pressedKeys.length > 1 && done;
+
+    const getKeyDisplay = (key) => {
+        if (key === 'arrowleft') return '←';
+        if (key === 'arrowright') return '→';
+        if (key === 'arrowrup') return '↑';
+        if (key === 'arrowrdown') return '↓';
+        return key;
+    };
+
+    const getNextKeys = () => {
+        const availableKeystrokeKeys = Object.keys(availableKeystrokes || {});
+        const shift = pressedKeys.length * 80 + pressedKeys.length * 20;
+        return (
+            <div className="next-keys-container">
+                {availableKeystrokeKeys.map((key) => {
+                    return (
+                        <div className="next-key" key={`ak-${key}`} style={{ marginLeft: shift }}>
+                            <div className="key-button">{getKeyDisplay(key)}</div>
+                            {/* <div>{JSON.stringify(availableKeystrokes[key])}</div> */}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
+
+    const getKeystrokeData = () => {
+        let os = availableKeystrokes?.Windows && 'Windows';
+        if (availableKeystrokes?.Mac) os = availableKeystrokes?.Mac && 'Mac';
+        return (
+            <div className="keystroke-description">
+                <div className="message">
+                    Chrome ({os}): {availableKeystrokes?.Chrome}
+                </div>
+            </div>
+        );
+    };
+
+    const getAvailableMessage = () => {
+        return (
+            <div className="keystroke-description">
+                <div className="green-message">The keystroke is available.</div>
+            </div>
+        );
+    };
 
     return (
-        <div>
-            <div>
-                {pressedKeyStroke}: {keystrokeDesc?.desc ? JSON.stringify(keystrokeDesc) : ''}
+        <div className="keys-presentation-container">
+            <div className="keystroke-description">
+                <div className="green-message">
+                    Press a keystroke to check its availability (currently only for chrome keystrokes).
+                </div>
             </div>
+            <div className={`keys-container ${available && 'available'}`}>
+                {Object.keys(pressedKey || {})?.length !== 0 &&
+                    pressedKeys.map((key) => {
+                        return (
+                            <span key={key} className="key-button">
+                                {getKeyDisplay(key)?.toLowerCase()}
+                            </span>
+                        );
+                    })}
+            </div>
+            <div>{!isKeystroke && getNextKeys()}</div>
+            <div>{isKeystroke && getKeystrokeData()}</div>
+            <div>{!isKeystroke && available && getAvailableMessage()}</div>
         </div>
     );
 };
