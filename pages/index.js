9d425e7bde573ab5821a8d84d084154610c3879d
Tem Ab <t3mir1an@gmail.com>
2023-04-02 19:21:41 +0000

    add rel="noreferrer"

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
             <section className={indexStyles.sectionWrapper}>
                 <h1>
                     <span className={indexStyles.textBG}>
-                        tem ab - software engineer <a href='https://twitter.com/bedrockcomputer' target='_blank'>@bedrockcomputer</a>
+                        tem ab - software engineer <a rel="noreferrer" href='https://twitter.com/bedrockcomputer' target='_blank'>@bedrockcomputer</a>
                     </span>
                 </h1>
                 <article>
@@ -42,12 +42,12 @@ export default function Home() {
                         <li>
                             
                     <span className={indexStyles.textBG}>
-                            <a href='https://github.com/tem-tem'>@github</a>
+                            <a rel="noreferrer" href='https://github.com/tem-tem'>@github</a>
                     </span>
                         </li>
                         <li>
                     <span className={indexStyles.textBG}>
-                            <a href='https://twitter.com/tttteeeemmmm'>@twitter</a>
+                            <a rel="noreferrer" href='https://twitter.com/tttteeeemmmm'>@twitter</a>
                     </span>
                         </li>
                     </ul>
@@ -61,17 +61,17 @@ export default function Home() {
                     <ul>
                         <li>
                     <span className={indexStyles.textBG}>
-                            <a href='https://defaultshortcuts.com' target='_blank'>defaultshortcuts</a>  - browser default shortcuts look up
+                            <a rel="noreferrer" href='https://defaultshortcuts.com' target='_blank'>defaultshortcuts</a>  - browser default shortcuts look up
                     </span>
                         </li>
                         <li>
                     <span className={indexStyles.textBG}>
-                            <a href='https://www.google.com/url?q=https%3A%2F%2Fapps.apple.com%2Fus%2Fapp%2Flogs-pure-expense-tracker%2Fid6445955890&sa=D&sntz=1&usg=AOvVaw2-V019ktERWM_I6klZQVbP' target='_blank'>logs</a> - expense logger for ios
+                            <a rel="noreferrer" href='https://www.google.com/url?q=https%3A%2F%2Fapps.apple.com%2Fus%2Fapp%2Flogs-pure-expense-tracker%2Fid6445955890&sa=D&sntz=1&usg=AOvVaw2-V019ktERWM_I6klZQVbP' target='_blank'>logs</a> - expense logger for ios
                     </span>
                         </li>
                         <li>
                     <span className={indexStyles.textBG}>
-                            <a href='https://apps.apple.com/us/app/stop-working-back-hurts/id6446755293?mt=12' target='_blank'>stop working</a> - pomodoro timer for mac
+                            <a rel="noreferrer" href='https://apps.apple.com/us/app/stop-working-back-hurts/id6446755293?mt=12' target='_blank'>stop working</a> - pomodoro timer for mac
                     </span>
                         </li>
                     </ul>
@@ -85,12 +85,12 @@ export default function Home() {
                     <ul>
                         <li>
                     <span className={indexStyles.textBG}>
-                            <a href='https://www.behance.net/12tem' target='_blank'>behance</a> - visual design experiments
+                            <a rel="noreferrer" href='https://www.behance.net/12tem' target='_blank'>behance</a> - visual design experiments
                     </span>
                         </li>
                         <li>
                     <span className={indexStyles.textBG}>
-                            <a href='https://www.are.na/tem-ab' target='_blank'>are.na</a> - photography
+                            <a rel="noreferrer" href='https://www.are.na/tem-ab' target='_blank'>are.na</a> - photography
                     </span>
                         </li>
                     </ul>
